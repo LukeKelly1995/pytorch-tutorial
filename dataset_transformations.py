ab0926c6760641f1fe6fcd23b01cc2cccab7178b
Luke Kelly <luke.kelly141295@gmail.com>
2020-11-22 23:04:38 +0000

    examples of softmax and cross entropy

--- dataset_transformations.py
+++ dataset_transformations.py
@@ -8,7 +8,9 @@ import numpy as np
 
 class WineDataset(Dataset):
     def __init__(self, transform=None):
-        xy = np.loadtxt("data/wine.csv", delimiter=",", dtype=np.float32, skiprows=1)
+        xy = np.loadtxt(
+            "data/wine.csv", delimiter=",", dtype=np.float32, skiprows=1
+        )
         self.n_samples = xy.shape[0]
 
         # note that we do not convert to tensor here
